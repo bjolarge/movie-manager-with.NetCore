064d20d2783c25bd708d337abba86d66885a245c
unknown <bjolarge59@gmail.com>
2023-11-07 10:08:48 +0100

    first commit

---
+++ Data/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_Management_System.Models;
+
+namespace Movie_Management_System.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; } 
+        public DbSet<Genre> Genres { get; set; } 
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movie_Management_System.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Rating { get; set; }
+        public string Ticket { get; set; }
+        public string Country { get; set; }
+        // the movie should have more than one Genre.
+        public ICollection<Genre> Genres{ get; set; }
+        public int? GenreId { get; set; }
+        [NotMapped]
+        public IFormFile Photo { get; set; }
+    }
+}
